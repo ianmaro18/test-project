3492af69d0ac5b04fb1b125418e07bfa1667290a
Ian Calderon <ian.maro18@gmail.com>
2020-04-12 14:46:32 -0700

    First commit.

---
+++ Application/Greeter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application
+{
+	public class Greeter
+	{
+		public string SayHello()
+		{
+			return SayHelloName("World");
+		}
+
+		public string SayHelloName(string name)
+		{
+			return $"Hello {name}!";
+		}
+	}
+}
---
+++ Tests/GreeterTests.cs
@@ -0,0 +1,27 @@
+﻿using Application;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Tests
+{
+	public class GreeterTests
+	{
+		[Fact]
+		public void SayHello_Test()
+		{
+			var sut = new Greeter();
+			var actual = sut.SayHello();
+			Assert.Equal("Hello World!", actual);
+		}
+
+		[Fact]
+		public void SayHelloName_Test()
+		{
+			var sut = new Greeter();
+			var actual = sut.SayHelloName("John");
+			Assert.Equal("Hello John!", actual);
+		}
+	}
+}
